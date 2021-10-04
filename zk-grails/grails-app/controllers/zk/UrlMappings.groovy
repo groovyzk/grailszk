572bf0f366bb1f48be983b660688945951650b71
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2021-10-04 12:36:59 -0300

    Fix #2: exclude /static url

--- zk-grails/grails-app/controllers/zk/UrlMappings.groovy
+++ zk-grails/grails-app/controllers/zk/UrlMappings.groovy
@@ -1,10 +1,16 @@
 package zk
 
 class UrlMappings {
+    static excludes = ["/static/*"]
 
     static mappings = {
-        "/"(view:"/index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
+        "/"(view: "/index")
+        "500"(view: "/error")
+        "404"(view: "/notFound")
+        "/$controller/$action?/$id?(.$format)?" {
+            constraints {
+                // apply constraints here
+            }
+        }
     }
 }
