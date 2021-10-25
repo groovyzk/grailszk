64ace3a993666767bbdcd1b70d670c8b0f5136c2
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2021-10-25 14:25:11 -0300

    Load .zul files from classpath instead of plugin path (Fix #5)

--- zk-grails/grails-app/controllers/zk/UrlMappings.groovy
+++ zk-grails/grails-app/controllers/zk/UrlMappings.groovy
@@ -4,9 +4,6 @@ class UrlMappings {
     static excludes = ["/static/*"]
 
     static mappings = {
-        "/"(view: "/index")
-        "500"(view: "/error")
-        "404"(view: "/notFound")
         "/$controller/$action?/$id?(.$format)?" {
             constraints {
                 // apply constraints here
